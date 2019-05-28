b4255bead0d3521b6dd33f1b9292487ac8446c15
Jefferson Matheus <jefferson227@gmail.com>
2019-05-27 23:49:33 -0300

    asdfasdf

---
+++ src/components/AddEditStock.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-text-field v-model="product" label="Produto" required></v-text-field>
+
+    <v-text-field v-model="store" label="Local" required></v-text-field>
+
+    <v-text-field v-model="packing" label="Embalagem" required></v-text-field>
+
+    <v-select v-model="unitOfMeasure" :items="unitsOfMeasure" label="Unidade de medida" required></v-select>
+
+    <v-text-field v-model="unitPrice" label="Preço unitário" required></v-text-field>
+
+    <v-text-field v-model="priceByUnitOfMeasure" label="Preço por kg/litro" required></v-text-field>
+
+    <v-btn :disabled="!valid" color="success" @click="validate">Validate</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    product: '',
+    store: '',
+    packing: '',
+    unitOfMeasure: null,
+    unitsOfMeasure: ['Grama', 'Mililitro', 'Unidade'],
+    unitPrice: '',
+    priceByUnitOfMeasure: ''
+  }),
+
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '../views/Home.vue';
+import Stock from '../views/Stock.vue';
 
 Vue.use(Router);
 
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'stock',
+      component: Stock
     }
   ]
 });
