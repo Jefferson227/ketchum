7018351f4d6ad61db5d1aad6816f49cbbc7a5bbe
Jefferson Matheus <jefferson227@gmail.com>
2019-05-30 13:58:14 -0300

    Implement store for vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import './plugins/vuetify';
 import router from './router/router';
+import store from './store';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
